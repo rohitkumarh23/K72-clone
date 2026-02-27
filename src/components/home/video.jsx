c060827a55c57de01a6c6e3d9cc9c97e418cb00b
rohit kumar <rohit9780k@gmail.com>
2026-02-27 14:57:23 +0530

    responsive update

--- src/components/home/video.jsx
+++ src/components/home/video.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import myVideo from "/src/assets/83c745cf.mp4"
+import myVideo from '../../assets/83c745cf.mp4'
 
-const Video = ()=>{
-    return(
-        <div className='h-full w-full'>
-            <video className='w-full h-full object-cover' autoPlay playsInline loop muted src={myVideo}></video>
-        </div>
-    )
+const Video = () => {
+  return (
+   <div className='h-full w-full'>
+     <video className='w-full h-full object-cover' autoPlay playsInline loop muted src={myVideo}>
+
+     </video>
+    </div>
+  )
 }
 
 export default Video
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,11 +5,11 @@ import HomeBottomText from '../components/home/homeBottomText'
 
 const Home = () => {
     return (
-        <div className='text-white min-h-[100dvh] bg-green-500 overflow-hidden'>
-            <div className='w-full h-full bg-red-500 fixed'>
+        <div className='text-white min-h-[100dvh] bg-black overflow-hidden'>
+            <div className='w-screen h-full  fixed'>
                 <Video /> 
             </div>
-            <div className='min-h-[100dvh]  w-full relative pb-5 overflow-hidden flex flex-col justify-between'>
+            <div className='min-h-[100dvh]  w-screen relative pb-5 overflow-hidden flex flex-col justify-between items-center'>
                 <HomeHeroText />
                 <HomeBottomText />
 
