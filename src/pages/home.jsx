5bd1a5f514e69c3b3c3e292dfe4e82a56c9a5d9d
rohit kumar <rohit9780k@gmail.com>
2026-02-27 13:50:14 +0530

    res

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,8 +5,8 @@ import HomeBottomText from '../components/home/homeBottomText'
 
 const Home = () => {
     return (
-        <div className='text-white'>
-            <div className='w-full min-h-[100dvh] fixed'>
+        <div className='text-white min-h-[100dvh] bg-green-500'>
+            <div className='w-full h-full bg-red-500 fixed'>
                 <Video /> 
             </div>
             <div className='min-h-[100dvh]  w-full relative pb-5 overflow-hidden flex flex-col justify-between'>
