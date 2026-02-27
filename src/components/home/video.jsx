812a517f9e55ae333b51539a922576db789cfe6e
rohit kumar <rohit9780k@gmail.com>
2026-02-27 12:51:46 +0530

    video path changed

--- src/components/home/video.jsx
+++ src/components/home/video.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import myVideo from "/src/assets/83c745cf.mp4"
 
 const Video = ()=>{
     return(
         <div className='h-full w-full'>
-            <video className='w-full h-full object-cover' autoPlay loop muted src="\src\assets\83c745cf.mp4"></video>
+            <video className='w-full h-full object-cover' autoPlay loop muted src={myVideo}></video>
         </div>
     )
 }
