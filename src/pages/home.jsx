11cf96ebeb10c5b66a067eeb7d911ab21264bc31
rohit kumar <rohit9780k@gmail.com>
2026-02-27 19:00:00 +0530

    background color changed

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,16 +5,14 @@ import HomeBottomText from '../components/home/homeBottomText'
 
 const Home = () => {
     return (
-        <div className='text-white min-h-[100dvh] overflow-hidden'>
-            <div className='w-screen h-full bg-black  fixed'>
-                <Video /> 
+        <div className='text-white min-h-[100dvh] bg-black overflow-hidden'>
+            <div className='w-full h-full bg-black  fixed'>
+                <Video />
             </div>
-            <div className='min-h-[100dvh]  w-screen relative pb-5 overflow-hidden flex flex-col justify-between items-center'>
+            <div className='min-h-[100dvh]   w-full relative pb-5 overflow-hidden flex flex-col justify-between items-center'>
                 <HomeHeroText />
                 <HomeBottomText />
-
             </div>
-
         </div>
     )
 }
